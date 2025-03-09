d756d5a252842b4d1676c35477a9b2276739617b
Shadow-Diamond <elementalpaladingaming@gmail.com>
2025-03-08 23:17:59 -0600

    FINAL UPDATE 2

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,6 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
+	GameManager.score = 0
+	GameManager.numDeaths = 0
 
 
 func _on_button_pressed():
